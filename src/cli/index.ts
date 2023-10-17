4c21d0c6178f73209ba1b57f265a489027850f4f
from-nibly <jordan@davidson.house>
2023-10-17 11:06:36 -0600

    Add initial get all command

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,10 +1,32 @@
 import { Command, Option, runExit } from 'clipanion';
+import wretch from 'wretch';
+
+const client = wretch('http://localhost:3000');
 
 export class GetCommand extends Command {
-  name = Option.String('-n,--name', { required: true });
+  static paths = [['get'], ['g']];
+  kind = Option.String();
+  name = Option.String({ required: false });
 
   async execute(): Promise<number | void> {
-    this.context.stdout.write(`Hello, world!  ${this.name}\n`);
+    let kind = this.kind;
+    let name = this.name;
+    let rest = [];
+
+    if (name === undefined && kind.indexOf('/') !== -1) {
+      [kind, name, ...rest] = kind.split('/');
+    }
+
+    if (name === undefined) {
+      //execute get all
+      const resp = await client.url(`/namespaces/foo/${kind}`).get().json();
+      this.context.stdout.write(
+        `Hello, world! ${kind} ${JSON.stringify(resp, null, 2)}\n`,
+      );
+    } else {
+      //execute get one
+      this.context.stdout.write(`Hello, world! ${kind} ${name}\n`);
+    }
   }
 }
 
